004cace266ecb018b45b6cccc8aef3ed602713b7
tk-tanab <tk-tanab@ist.osaka-u.ac.jp>
2022-12-18 16:16:13 +0900

    delete pycache

--- debiantospdx/test.py
+++ debiantospdx/test.py
@@ -1,3 +1,15 @@
 # 2022-11-21T03:49:09Z
 # YYYY-MM-DDThh:mm:ssZ
-print("test ")
+import os
+import pathlib
+import shutil
+
+
+def remove_glob(dirname, pathname):
+    path_object = pathlib.Path(pathname)
+    for p in path_object.glob("**"):
+        if os.path.isdir(p) and dirname == os.path.basename(p):
+            shutil.rmtree(p)
+
+
+remove_glob("__pycache__", ".venv")
